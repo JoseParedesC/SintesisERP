44b11b24989b8e839bccd2028035dac0ac8c1438
JPAREDES <jparedes@sintesiscloud.com>
2021-02-18 15:52:53 -0500

    JPAREDES scripts

--- Database/Scripts/ServicePack/Version 1.2.2/Sctructure/202102051627_Update_FormaPagos.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
---changeset ,JPAREDES:1 dbms:mssql runOnChange:true endDelimiter:GO stripComments:false
-IF NOT EXISTS(SELECT 1 FROM FormaPagos F INNER JOIN CNTCuentas C ON C.id = F.id_cuenta WHERE F.nombre = 'EFECTIVO CAJAS' AND C.codigo = 5235)
-UPDATE F 
-	SET F.id_cuenta = (SELECT id FROM CNTCuentas WHERE codigo = 5235)
-FROM FormaPagos AS F WHERE nombre = 'EFECTIVO CAJAS'
